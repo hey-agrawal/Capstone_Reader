904555456f566f760ed8bfa1ccd1ffa9077e846e
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-07-26 09:37:57 +0530

    data save in firebase

--- app/src/main/java/com/example/areader/MainActivity.kt
+++ app/src/main/java/com/example/areader/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.areader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +12,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.areader.ui.theme.AReaderTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AReaderTheme {
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user ["FirstName"] = "Joe"
+                user ["lastName"] = "James"
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("FB", "onCreate: ${it.id}")
+                        }.addOnFailureListener {
+                            Log.d("FB", "onCreate: $it")
+                        }
                     Greeting("Android")
                 }
             }
