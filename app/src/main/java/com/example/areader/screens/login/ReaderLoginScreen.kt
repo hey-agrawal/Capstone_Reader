785b3d7fb6dbbd1e1d63a8e9ee939bf00d86c8d9
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-07-29 09:40:46 +0530

    Setting up the user form

--- app/src/main/java/com/example/areader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/areader/screens/login/ReaderLoginScreen.kt
@@ -1,5 +1,7 @@
 package com.example.areader.screens.login
 
+import android.accounts.Account
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,10 +13,13 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -24,15 +29,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.areader.components.EmailInput
+import com.example.areader.components.PasswordInput
 import com.example.areader.components.ReaderLogo
 import kotlin.math.sin
 
@@ -43,13 +52,20 @@ fun ReaderLoginScreen(navController: NavController){
             verticalArrangement = Arrangement.Top
             ) {
             ReaderLogo()
+            UserForm(loading = false, isCreateAccount = false){email, password ->
+                Log.d("Form","ReaderLoginScreen: $email $password")
+            }
         }
     }
 }
 @OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
-fun UserForm(){
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone : (String, String) -> Unit = {email, pwd->}
+){
     val email = rememberSaveable{ mutableStateOf("") }
     val password = rememberSaveable() { mutableStateOf("") }
     val passVisibility = rememberSaveable() { mutableStateOf(false) }
@@ -64,7 +80,19 @@ fun UserForm(){
         .verticalScroll(rememberScrollState())
 
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-            EmailInput(emailState = email, enabled = true, onAction = KeyboardActions { passwordFocusRequest.requestFocus() })
+            EmailInput(emailState = email, enabled = !loading, onAction = KeyboardActions { passwordFocusRequest.requestFocus() })
     }
+    PasswordInput(
+        modifier = Modifier.focusRequester(passwordFocusRequest),
+        passwordState = password,
+        labelId = "Password",
+        enabled = !loading,
+        passVisibility = passVisibility,
+        onAction = KeyboardActions{
+            if (!valid) return@KeyboardActions
+            onDone(email.value.trim(), password.value.trim())
+
+        }
+    )
 }
 
