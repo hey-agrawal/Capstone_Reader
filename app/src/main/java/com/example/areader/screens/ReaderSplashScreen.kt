f1780f0138690ca1b72590a0bbc0a250e17b4868
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-07-29 18:02:29 +0530

    creating MUser Model

--- app/src/main/java/com/example/areader/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/areader/screens/ReaderSplashScreen.kt
@@ -48,12 +48,13 @@ fun ReaderSplashScreen(navController: NavController) {
         )
         delay(2000L)
 
-        if (FirebaseAuth.getInstance().currentUser?.email.isNullOrEmpty()) {
-
-            navController.navigate(ReaderScreens.LoginScreen.name)
-        } else {
-            navController.navigate(ReaderScreens.ReaderHomeScreen.name)
-        }
+//        if (FirebaseAuth.getInstance().currentUser?.email.isNullOrEmpty()) {
+//
+//            navController.navigate(ReaderScreens.LoginScreen.name)
+//        } else {
+//            navController.navigate(ReaderScreens.ReaderHomeScreen.name)
+//        }
+        navController.navigate(ReaderScreens.LoginScreen.name)
     }
     Surface(
         modifier = Modifier
