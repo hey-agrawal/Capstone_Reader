f429239ef7cd0372744d4c2550e6dec653d1953d
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-07-29 10:40:48 +0530

    Create Account ui

--- app/src/main/java/com/example/areader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/areader/screens/login/ReaderLoginScreen.kt
@@ -3,13 +3,17 @@ package com.example.areader.screens.login
 import android.accounts.Account
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -35,7 +39,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -44,6 +50,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.areader.R
 import com.example.areader.components.EmailInput
 import com.example.areader.components.PasswordInput
 import com.example.areader.components.ReaderLogo
@@ -51,14 +58,38 @@ import kotlin.math.sin
 
 @Composable
 fun ReaderLoginScreen(navController: NavController){
+    val showLoginForm = rememberSaveable{ mutableStateOf(true) }
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
             ) {
             ReaderLogo()
-            UserForm(loading = false, isCreateAccount = false){email, password ->
-                Log.d("Form","ReaderLoginScreen: $email $password")
+            if(showLoginForm.value) UserForm(loading = false, isCreateAccount = false){email, password ->
+                //TODO FB Login
             }
+            else {
+                UserForm(loading = false, isCreateAccount = true ){
+                        email, password ->
+                    //Todo create FB account
+
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Row(modifier = Modifier.padding(15.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically) {
+            val text = if (showLoginForm.value) "Sign up" else "Login"
+            Text(text = "New User?")
+            Text(text,
+            modifier = Modifier
+                .clickable {
+                    showLoginForm.value = !showLoginForm.value
+                }
+                .padding(start = 5.dp),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.secondaryVariant)
         }
     }
 }
@@ -84,6 +115,9 @@ fun UserForm(
         .verticalScroll(rememberScrollState())
 
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        if (isCreateAccount) Text(text = stringResource(id = R.string.create_acct),
+        modifier = Modifier.padding(4.dp))
+        else Text("")
         EmailInput(
             emailState = email,
             enabled = !loading,
@@ -109,6 +143,7 @@ fun UserForm(
 
             ){
                 onDone(email.value.trim(), password.value.trim())
+                keyboardController?.hide()
             }
     }
 }
